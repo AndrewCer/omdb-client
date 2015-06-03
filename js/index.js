4cf2dfb92d4993a9c8dbb1222acaff2004ec489b
Andrew Cer <cer.andrew@gmail.com>
2015-06-03 17:04:02 -0600

    initial setup

---
+++ js/index.js
@@ -0,0 +1,5 @@
+var h1 = document.createElement('h1');
+var header = document.getElementById('header');
+h1.className = "main-header";
+h1.innerHTML = "Please pick a movie";
+header.appendChild(h1);
